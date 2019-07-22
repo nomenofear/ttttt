6130ac19bcc207bffe718d5231185f20d5b6ba40
Pino <guanpeng.li@ucarinc.com>
2019-07-22 08:51:41 +0800

    first commit

---
+++ src/main/java/com/oops/myproj/myproj_inherit/App.java
@@ -0,0 +1,13 @@
+package com.oops.myproj.myproj_inherit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
